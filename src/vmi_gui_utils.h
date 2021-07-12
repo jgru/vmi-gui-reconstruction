a2b10db36d61bf0d52d211238222464e1b175851
Jan Gru <j4n6ru@gmail.com>
2021-07-12 12:35:33 +0000

    Add explainations on the calculation of the _OBJECT_HEADER_NAME_INFO

--- src/vmi_gui_utils.h
+++ src/vmi_gui_utils.h
@@ -1,6 +1,6 @@
 /* vmi-reconstruct-gui - a tool to reconstruct the GUI of a running VM.
  *
- * Copyright (C) 2021 Jan Gru <j4n6ru@gmail.com>
+ * Copyright (C) 2021 Jan Gruber <j4n6ru@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,23 +29,30 @@
 
 #include "vmi_win_offsets.h"
 
-
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
 /*
- * Checks, wether the _OBJECT_HEADER, which precedes every executive object,
+ * Checks, whether the _OBJECT_HEADER, which precedes every executive object,
  * is preceded by an optional header of type _OBJECT_HEADER_NAME_INFO.
  * If this is the case, the name of the executive object is read and returned.
+ *
  */
 const char* retrieve_objhdr_name(vmi_instance_t vmi, addr_t addr)
 {
     addr_t obj_hdr = 0;
     addr_t obj_hdr_nameinfo_addr= 0;
-    //addr_t name_addr = 0;
     uint8_t im = 0;
     const char* name = NULL;
     unicode_string_t* us = NULL;
     unicode_string_t out = { .contents = NULL };
 
+    /*
+     * Retrieves the beginning of the _OBJECT_HEADER, to find it subtract the
+     * offset to the body from current address. Since the current executive
+     * object is _partly_ incorporated in the size of the _OBJECT_HEADER-struct
+     *
+     * See "The Art of Memory Forensics", p. 119 ff. for a description
+     */
     obj_hdr = addr - off.objhdr_body_offset;
     obj_hdr_nameinfo_addr = obj_hdr;
 
@@ -56,11 +63,15 @@ const char* retrieve_objhdr_name(vmi_instance_t vmi, addr_t addr)
         return NULL;
     }
 
-    /* Checks, if there is a _OBJECT_HEADER_CREATOR_INFO after *NAME_INFO */
+    /*
+     * Checks, if there comes an optional _OBJECT_HEADER_CREATOR_INFO after
+     * _OBJECT_HEADER_NAME_INFO, which has to added to the offset to subtract
+     * from the address signifying the start of the _OBJECT_HEADER
+     */
     if (im & OBJ_HDR_INFOMASK_CREATOR_INFO)
         obj_hdr_nameinfo_addr -= off.objhdr_creator_info_length;
 
-    /* Returns, if there is no _OBJECT_HEADER_NAME_INFO */
+    /* Returns NULL immediately, if there is no _OBJECT_HEADER_NAME_INFO */
     if (!(im & OBJ_HDR_INFOMASK_NAME))
         return NULL;
 
@@ -81,7 +92,7 @@ const char* retrieve_objhdr_name(vmi_instance_t vmi, addr_t addr)
 }
 
 /*
- * Read a Windows wchar-string into a wchar_t*, since vmi_read_unicode_str_va
+ * Reads a Windows wchar-string into a wchar_t*, since vmi_read_unicode_str_va
  * fails to parse _RTL_ATOM_ENTRY's name-string or _LARGE_UNICODE_STRINGs.
  * Expansion is performed since Windows' wchar is 2 bytes versus 4 bytes on
  * 64bit-Linux
